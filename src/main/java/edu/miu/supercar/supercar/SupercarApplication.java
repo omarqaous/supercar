1c364548d97ad5c055f9669b6c62a379b01e2d3a
omarqaous <omarqaous@gmail.com>
2024-04-12 10:15:39 -0500

    first commit

---
+++ src/main/java/edu/miu/supercar/supercar/SupercarApplication.java
@@ -0,0 +1,12 @@
+package edu.miu.supercar.supercar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SupercarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupercarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/supercar/supercar/SupercarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.supercar.supercar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
